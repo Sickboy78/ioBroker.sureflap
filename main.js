e7c97e0db8bfd6988a85ab4c438d1caaa2b67825
Sickbart <sickbart@gmx.de>
2023-06-01 22:02:21 +0200

    v1.1.8

--- main.js
+++ main.js
@@ -243,7 +243,7 @@ class Sureflap extends utils.Adapter {
 	 * starts loading data from the surepet API
 	 */
 	startLoadingData() {
-		this.log.debug(`starting SureFlap Adapter v1.1.7`);
+		this.log.debug(`starting SureFlap Adapter v1.1.8`);
 		clearTimeout(this.timerId);
 		this.doAuthenticate()
 			.then(() => this.startUpdateLoop())
